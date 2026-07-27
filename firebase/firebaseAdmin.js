887794fed5c20f7e8a1cda6d17be0d02215620bc
Silas <silasmomoh2017@gmail.com>
2026-07-27 05:29:36 +0100

    Use environment variables for Firebase Admin

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -2,11 +2,18 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-import { readFileSync } from "fs";
-
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+const serviceAccount = {
+  type: "service_account",
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_CLIENT_ID,
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+  tokenUri: "https://oauth2.googleapis.com/token",
+  authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+  clientC509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 if (!getApps().length) {
   initializeApp({
